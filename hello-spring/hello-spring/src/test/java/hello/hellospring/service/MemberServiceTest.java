f61c77c5ff8e2d5c91aea7e6b159add35426d08b
Jaeman Kim <daga4242@gmail.com>
2023-09-17 23:22:18 +0900

    test and service

---
+++ hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+
+    @Test
+    void regist(){
+        //given
+
+        //when
+
+        //then
+    }
+}
