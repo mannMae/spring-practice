c7e053e47604c34a46f1b0058375b581ef4f65f6
Jaeman Kim <daga4242@gmail.com>
2023-09-21 23:47:45 +0900

    jpa

--- hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -41,16 +41,16 @@ class MemberServiceIntegrationTest {
     public void 중복_회원_예외() {
         //given
         Member member1 = new Member();
-        member1.setName("spring22");
+        member1.setName("spring");
 
         Member member2 = new Member();
-        member2.setName("spring233");
+        member2.setName("spring");
 
         //when
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, ()-> memberService.join(member2));
 
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원이름입니다.");
 
     }
 }
